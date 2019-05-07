ea49d52d3e7146059ec467b6187cbc26c2af5173
id_idChan <95123129@qq.com>
2019-05-07 16:56:00 +0800

    edit_02(final edition) 2019/5/7 16:55

--- text-file-process/counter.py
+++ text-file-process/counter.py
@@ -2,10 +2,14 @@ import os
 
 counter = 0
 
-for files in os.walk(r'C:\Users\95123\Desktop\text-file-process-ididChan\text-file-process\log_files'):
-    for file in files:
-        stu = file.split(',')[1]
-        stu_id = stu[13:]
-        if stu_id == '29':
-            counter += 1
+path = r'C:\Users\95123\Desktop\text-file-process-ididChan\text-file-process\log_files'
+files = os.listdir(path)
+for file in files:
+    with open(path + '//' + file,encoding='utf8') as f:
+        for line in f:
+            stu = line.split(',')[1]
+            stu_id = stu[14:]
+            if stu_id == '29':
+                counter =counter + 1
+
 print(counter)
